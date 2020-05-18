e145e06dabf5c0ecd0b06063f0c18e7e7e8dd21c
tomglynnejones <thomas.glynne-jones@metaswitch.com>
2020-05-18 15:52:30 +0100

    Initial dotnet console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace orca_chess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
